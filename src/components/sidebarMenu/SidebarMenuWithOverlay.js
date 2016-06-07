ecc7d38946788c82b24654f040b20d9eb528967f
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-06-07 16:50:55 +0200

    Added the overlay to the sidebar

---
+++ src/components/sidebarMenu/SidebarMenuWithOverlay.js
@@ -0,0 +1,51 @@
+import React, {Component} from "react";
+import CSSModules from "react-css-modules";
+import styles from "./sidebarMenu.css";
+
+class SidebarMenuWithOverlay extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {width: 0};
+	}
+
+	componentDidMount() {
+		this.setSidebarWidth();
+	}
+
+	setSidebarWidth() {
+		this.setState({
+			width: this.refs.sidebar.offsetWidth
+		});
+	}
+
+	getStyle() {
+		return this.state.width
+			? {left: this.props.hidden	? -this.state.width	: 0}
+			: {}
+	}
+
+	render() {
+		return (
+			<div>
+				<div styleName="sidebar" style={this.getStyle()} ref="sidebar">
+					{this.props.children}
+				</div>
+				<div styleName="overlay">
+				</div>
+			</div>
+
+		)
+	}
+}
+
+SidebarMenuWithOverlay.propTypes = {
+	hidden: React.PropTypes.bool,
+	children: React.PropTypes.node
+};
+
+SidebarMenuWithOverlay.defaultProps = {
+	hidden: true
+};
+
+export default CSSModules(SidebarMenuWithOverlay, styles);
---
+++ test/components/sidebarMenu/sidebarMenuOverlay_test.js
@@ -0,0 +1,65 @@
+import React from "react";
+//import ReactDOM from "react-dom";
+import {shallow, mount} from "enzyme";
+import SidebarMenu from "../../../src/components/sidebarMenu/SidebarMenuWithOverlay";
+import sidebarStyles from "../../../src/components/sidebarMenu/sidebarMenu.css";
+
+describe("<SidebarMenuOverlay />", function () {
+	it("has a sidebar element", function () {
+		const sidebarMenu = shallow(<SidebarMenu />),
+					sidebar = sidebarMenu.find(`.${sidebarStyles.sidebar}`);
+
+		assert.equal(sidebar.length, 1);
+	});
+
+	it("has an overlay element", function () {
+		const sidebarMenu = shallow(<SidebarMenu />),
+					overlay = sidebarMenu.find(`.${sidebarStyles.overlay}`);
+
+		assert.equal(overlay.length, 1);
+	});
+
+	it("defaults to hidden", function () {
+		const sidebarMenu = mount(<SidebarMenu />);
+
+		assert.equal(sidebarMenu.prop("hidden"), true);
+	});
+
+	//
+	// it("calculates its width dynamically when mounted", function () {
+	// 	const sidebar  = ReactDOM.render(<SidebarMenu>Test</SidebarMenu>, document.body),
+	// 				style    = window.getComputedStyle(ReactDOM.findDOMNode(sidebar)),
+	// 				domWidth = Math.round(parseFloat(style.getPropertyValue("width")));
+	//
+	// 	ReactDOM.unmountComponentAtNode(document.body);
+	//
+	// 	assert.equal(sidebar.state.width, domWidth);
+	// });
+	//
+
+	//
+	//
+	// it("is positioned to the left when hidden", function () {
+	// 	const sidebar               = ReactDOM.render(<SidebarMenu>Test</SidebarMenu>, document.body),
+	// 				sidebarDOMNode        = ReactDOM.findDOMNode(sidebar),
+	// 				sidebarTopRightCorner = sidebarDOMNode.getBoundingClientRect().right;
+	//
+	// 	ReactDOM.unmountComponentAtNode(document.body);
+	//
+	// 	assert.isBelow(sidebarTopRightCorner, 0);
+	// });
+	//
+	// it("is visible when hidden is false", function () {
+	// 	const sidebar              = ReactDOM.render(<SidebarMenu hidden={false}>Test</SidebarMenu>, document.body),
+	// 				sidebarDOMNode       = ReactDOM.findDOMNode(sidebar),
+	// 				sidebarTopLeftCorner = sidebarDOMNode.getBoundingClientRect().left;
+	//
+	// 	ReactDOM.unmountComponentAtNode(document.body);
+	//
+	// 	assert.equal(sidebarTopLeftCorner, 0);
+	// });
+});
+
+
+
+
