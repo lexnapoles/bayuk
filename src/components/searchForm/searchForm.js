ac78491c4999faca8b4bfa0892fb6c429ff39967
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-06-29 20:03:12 +0200

    Finished very simple form.

--- src/components/searchForm/searchForm.js
+++ src/components/searchForm/searchForm.js
@@ -21,9 +21,45 @@ class SearchForm extends Component {
 				</header>
 				<main className={main}>
 					<form styleName="searchForm">
-						<input placeholder="Name" />
-						<input placeholder="Category" />
-						<input placeholder="Price" />
+						<div styleName="filter">
+							<label htmlFor="name" styleName="title">Name</label>
+							<hr/>
+							<input id="name" type="text" placeholder="Name" />
+						</div>
+						<div styleName="filter">
+							<label styleName="title">Category</label>
+							<hr/>
+							<div>
+								<div>
+									<label htmlFor="Music">Music</label>
+									<input id="Music" type="checkbox"/>
+								</div>
+								<div>
+									<label htmlFor="Videogames">Videogames</label>
+									<input id="Videogames" type="checkbox"/>
+								</div>
+								<div>
+									<label htmlFor="Movies">Movies</label>
+									<input id="Movies" type="checkbox"/>
+								</div>
+								<div>
+									<label htmlFor="Literature">Literature</label>
+									<input id="Literature" type="checkbox"/>
+								</div>
+							</div>
+						</div>
+						<div styleName="filter">
+							<label styleName="title">Price</label>
+							<hr/>
+							<div >
+								<label htmlFor="minPrice">Min price</label>
+								<input id="minPrice" type="number" min="0" step="0.01" />
+							</div>
+							<div>
+								<label htmlFor="maxPrice">Max price</label>
+								<input id="maxPrice" type="number" min="0" step="0.01" />
+							</div>
+						</div>
 					</form>
 				</main>
 			</div>
