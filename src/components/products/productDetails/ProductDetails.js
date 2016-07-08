d2a5f0e5b30587b74ddac6e0cdedcff8b95b629d
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-07-08 10:22:26 +0200

    Made route to the details of a product. Created ProductDetails component.

---
+++ src/components/products/productDetails/ProductDetails.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+
+class ProductDetails extends Component {
+	render() {
+		return (
+			<div>
+				<header>
+					Header
+				</header>
+
+				<section>
+					Photos
+				</section>
+
+
+				<section>
+					Product Info
+				</section>
+
+				<section>
+					Geolocation
+				</section>
+
+				<section>
+					User
+				</section>
+			</div>
+		);
+
+
+	}
+}
+
+export default ProductDetails;
