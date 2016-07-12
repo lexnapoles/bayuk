ad2914937a8ca7d2db2904bcbbf86665cfee74d0
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-07-12 10:14:06 +0200

    Converted SearchFormHeader into a stateless function

--- src/components/searchForm/searchFormHeader/SearchFormHeader.js
+++ src/components/searchForm/searchFormHeader/SearchFormHeader.js
@@ -1,27 +1,21 @@
-import React, {Component} from "react";
-import CSSModules from "react-css-modules";
-import styles from "./searchFormHeader.css";
+import React from "react";
+import {check, checkButton} from "./searchFormHeader.css";
 import Icon from "react-fa";
 import Header from "../../header/Header";
 import ReturnIcon from "../../icons/returnIcon/ReturnIcon";
 
-class SearchHeader extends Component {
-	render() {
-		return (
-			<Header>
-					<ReturnIcon url="/"/>
-					<div styleName="check">
-						<button styleName="checkButton" form={this.props.formId}>
-							<Icon name="check" size="lg"/>
-						</button>
-					</div>
-			</Header>
-		);
-	}
-}
+const SearchHeader = ({formId}) =>
+	<Header>
+		<ReturnIcon url="/"/>
+		<div className={check}>
+			<button className={checkButton} form={formId}>
+				<Icon name="check" size="lg"/>
+			</button>
+		</div>
+	</Header>
 
 SearchHeader.propTypes = {
 	formId: React.PropTypes.string.isRequired
 }
 
-export default CSSModules(SearchHeader, styles);
+export default SearchHeader;
