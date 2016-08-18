355fbd8a0073b3234f8356f25a1fde6ea992c42f
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-08-18 19:38:18 +0200

    Extracted product description into its own component

--- src/components/products/productDetails/ProductDetails.js
+++ src/components/products/productDetails/ProductDetails.js
@@ -4,6 +4,7 @@ import styles from "./productDetails.css";
 
 import ProductDetailsHeader from "./productDetailsHeader/ProductDetailsHeader";
 import Carousel from "nuka-carousel";
+import ProductDescription from "./productDescription/ProductDescription";
 import Icon from "react-fa";
 
 class ProductDetails extends Component {
@@ -21,24 +22,7 @@ class ProductDetails extends Component {
 					</section>
 
 					<div styleName="productInfo">
-						<section styleName="description">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam elementum sed ligula sed sollicitudin.
-							Nullam fringilla diam eget lorem auctor hendrerit. Phasellus in nunc at nisi maximus volutpat.
-							Vivamus pellentesque est eu enim tempor dignissim et nec arcu. Proin sed faucibus odio. Maecenas ac
-							euismod
-							nulla.
-							In ut facilisis nunc. Donec tincidunt urna quis quam faucibus faucibus. Sed vel risus mattis, interdum
-							arcu
-							sed, auctor lorem.
-
-							Nam a metus a diam ullamcorper sodales dictum et ligula. Aliquam ultrices viverra suscipit.
-							Etiam nunc elit, luctus vitae lectus vulputate, feugiat feugiat felis. In non sem ornare, ornare purus ac,
-							volutpat urna.
-							Morbi vitae nibh tempor nisl aliquet porttitor. Proin commodo commodo nisi. Vestibulum mollis nunc at orci
-							tempus dignissim.
-							Duis a maximus tortor, eu condimentum ex. Vestibulum malesuada pulvinar ullamcorper.
-							Suspendisse ac purus sit amet dui varius mattis at eu tortor.
-						</section>
+						<ProductDescription />
 						<section styleName="geolocation">
 							<img styleName="map"
 								src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
