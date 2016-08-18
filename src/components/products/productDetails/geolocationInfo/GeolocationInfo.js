2d34d4e478907f5f6389be52640f1bb08057f417
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-08-18 20:09:09 +0200

    Added back the media queries for desktop.

--- src/components/products/productDetails/geolocationInfo/GeolocationInfo.js
+++ src/components/products/productDetails/geolocationInfo/GeolocationInfo.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {map} from "./geolocationInfo.css";
+import {container, map} from "./geolocationInfo.css";
 
 const GeolocationInfo = () => {
 	return (
-		<div>
+		<div className={container}>
 			<img className={map}
 				src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
 &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
