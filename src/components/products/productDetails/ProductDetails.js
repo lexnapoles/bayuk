4c8131a6467aa88e00079bb211106911c39bcae6
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-08-18 18:37:13 +0200

    Styled a basic mobile first product details page. Cleaned desktop style.

--- src/components/products/productDetails/ProductDetails.js
+++ src/components/products/productDetails/ProductDetails.js
@@ -39,14 +39,15 @@ class ProductDetails extends Component {
 							Duis a maximus tortor, eu condimentum ex. Vestibulum malesuada pulvinar ullamcorper.
 							Suspendisse ac purus sit amet dui varius mattis at eu tortor.
 						</section>
-
 						<section styleName="geolocation">
 							<img styleName="map"
-									src="http://maps.googleapis.com/maps/api/staticmap?center=51.455041,-0.9690884&zoom=17&size=400x100&sensor=false&markers=51.455041,-0.9690884&scale=2"/>
+								src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
+&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
+&markers=color:red%7Clabel:C%7C40.718217,-73.998284"/>
 						</section>
-
 						<section styleName="user">
-							<img styleName="userImage" width="100" height="100" src="http://www.publicdomainpictures.net/pictures/20000/velka/women-face.jpg" alt="userPic"/>
+							<img styleName="userImage" width="100" height="100"
+								src="http://www.publicdomainpictures.net/pictures/20000/velka/women-face.jpg" alt="userPic"/>
 							<p>John McStar</p>
 							<div styleName="rating">
 								<Icon name="star" size="lg"/>
