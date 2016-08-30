d27ab553a56f8d5507ba13c47ebc35b645ab0c76
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-08-30 17:50:31 +0200

    Added react-image-gallery to product details page

--- src/components/products/productDetails/ProductDetails.js
+++ src/components/products/productDetails/ProductDetails.js
@@ -3,7 +3,7 @@ import CSSModules from "react-css-modules";
 import styles from "./productDetails.css";
 
 import ProductDetailsHeader from "./productDetailsHeader/ProductDetailsHeader";
-import Carousel from "nuka-carousel";
+import Carousel from "../../carousel/Carousel";
 import Description from "./productDescription/ProductDescription";
 import GeolocationInfo from "./geolocationInfo/GeolocationInfo";
 import UserInfo from "./userInfo/UserInfo";
@@ -14,14 +14,11 @@ class ProductDetails extends Component {
 			<div styleName="container">
 				<ProductDetailsHeader/>
 				<main styleName="productContainer">
-					<section styleName="carousel">
-						<Carousel>
-							<img src="http://placehold.it/200x200"/>
-							<img src="http://placekitten.com/500/1000"/>
-							<img src="http://placekitten.com/500/900"/>
-							<img src="http://placehold.it/200x200"/>
+						<Carousel styleName="carousel">
+							<img src="http://placehold.it/200x200?text=slide1"/>
+							<img src="http://placehold.it/200x200?text=slide2"/>
+							<img src="http://placehold.it/200x200?text=slide3"/>
 						</Carousel>
-					</section>
 
 					<section styleName="info">
 						<Description styleName="infoSection"/>
