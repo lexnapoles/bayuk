059a5bbc3cf1db6d60661e2882e2396c6daba19e
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-09-05 18:01:30 +0200

    Added Provider to routes.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import "normalize.css/normalize.css";
 import React from "react";
 import {render} from "react-dom";
 import {Router, Route, browserHistory} from "react-router";
-
+import {Provider} from "react-redux";
+import store from "./store/store";
 import App from "./components/app/App";
 import SearchFormContainer from "./components/searchForm/SearchFormContainer";
 import ProductDetails from "./components/products/productDetails/ProductDetails";
@@ -12,10 +13,10 @@ if (module.hot) {
 	module.hot.accept();
 }
 
-render((
+render((<Provider store={store}>
 	<Router history={browserHistory}>
-		<Route path="/" component={App} />
-		<Route path="search" component={SearchFormContainer} />
-		<Route path="product" component={ProductDetails} />
+		<Route path="/" component={App}/>
+		<Route path="search" component={SearchFormContainer}/>
+		<Route path="product" component={ProductDetails}/>
 	</Router>
-), document.querySelector(".app"));
+</Provider>), document.querySelector(".app"));
