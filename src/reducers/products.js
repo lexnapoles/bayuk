7b014da0200345738a8879f55b502ef8d71b232a
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-09-16 18:54:26 +0200

    Products are centered in small screens.

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,9 +1,3 @@
 export const getProductById = (state, id) => {
-	window.console.log("getProduct");
-	window.console.log(state);
-	window.console.log(id);
-
-
-	window.console.log(state.find(product => product.id === id));
-		return state.find(product => product.id === id);
+	return state.find(product => product.id === id);
 }
