42cfb9b45c6511b1c6d114aa3848487aebdbabff
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-10-14 18:30:51 +0200

    Users can see thumbnails of selected images

--- src/components/inputs/imgInput/ImgInput.js
+++ src/components/inputs/imgInput/ImgInput.js
@@ -6,15 +6,27 @@ class ImgInput extends Component {
 		super(props);
 
 		this.state = {
-			images: []
+			images: [],
+			urls:   []
 		};
 
 		this.handleFiles = this.handleFiles.bind(this);
-		this.isImage= this.isImage.bind(this);
 	}
 
 	isImage(file) {
 		return /^image\//.test(file.type);
+	}
+
+	readUrlsFromImages(images) {
+		images.forEach(image => {
+			const reader = new FileReader();
+
+			reader.onload = event => this.setState({
+				urls: [...this.state.urls, event.target.result]
+			});
+
+			reader.readAsDataURL(image);
+		})
 	}
 
 	handleFiles() {
@@ -23,11 +35,22 @@ class ImgInput extends Component {
 		const images = selectedFiles.filter(file => this.isImage(file));
 
 		this.setState({images});
+
+		this.readUrlsFromImages(images);
+	}
+
+	getImages() {
+		const imgUrls = this.state.urls;
+
+		return imgUrls.map(url => <img id={url} src={url} className="thubmnail"/>);
 	}
 
 	render() {
 		return (
 			<Filter title="Pictures">
+				<div className="preview">
+					{this.getImages()}
+				</div>
 				<input type="file" multiple accept="image/*" ref="input" onChange={this.handleFiles}/>
 			</Filter>
 		)
