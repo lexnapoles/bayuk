5fca4baacb6500d4a244a3c2500d6e921df216e8
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-11-28 10:11:49 +0100

    Added params error checking to readOneProduct.

--- server/api/controllers/products.js
+++ server/api/controllers/products.js
@@ -1,11 +1,21 @@
 import {sendJsonResponse} from "../utils/utils";
 import {getProductById} from "../services/products";
 
+const paramExists = (req, param) => req.params && req.params[param];
+
 export const readProducts = (req, res) => {
 	sendJsonResponse(res, 200, {"status": "success"});
 };
 
 export const readOneProduct = (req, res) => {
+	if (!paramExists(req, "productId")) {
+		sendJsonResponse(res, 404, {
+			"message": "No productId in request"
+		});
+
+		return;
+	}
+
 	const {productId} = req.params;
 
 	getProductById(productId)
