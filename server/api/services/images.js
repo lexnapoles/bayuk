480d4a0ad612565ab97927504cb0e994f0227d81
Alejandro Napoles <alejandro.napoles@gmail.com>
2016-12-02 10:29:13 +0100

    Passing array of images objects to write the images to disk.

--- server/api/services/images.js
+++ server/api/services/images.js
@@ -8,6 +8,23 @@ const getDecodedImage = data => {
 	return new Buffer(img, "base64");
 };
 
+const writeOneImageToDisk = ({id, data}) => {
+	const imagePath = `${path.join(process.env.IMAGESDIR, "/products", id)}.jpg`;
+
+	return fs.writeFile(imagePath, getDecodedImage(data))
+		.then(() => id);
+};
+
+export const writeImagesToDisk = (images = []) => {
+	if (!images.length) {
+		return Promise.reject("No images has been passed");
+	}
+
+	const wrappedImagesInPromises = images.map(img => Promise.resolve(writeOneImageToDisk(img)));
+
+	return Promise.all(wrappedImagesInPromises);
+};
+
 const addOneImage = productId => {
 	if (!productId) {
 		return Promise.reject("Product id is not defined");
@@ -16,42 +33,14 @@ const addOneImage = productId => {
 	return db.one("INSERT into images (product_id) VALUES ($1) RETURNING image_id", productId)
 		.then(({image_id}) => image_id);
 };
-
-const writeOneImageToDisk = ({id, data}) => {
-	const imagePath = `${path.join(process.env.IMAGESDIR, "/products", id)}.jpg`;
-
-	return fs.writeFile(imagePath, getDecodedImage(data))
-		.then(() => id)
-};
-
-export const writeImagesToDisk = (images = []) => {
-	if (!images.length) {
-		return Promise.reject("No images has been passed");
-	}
-
-	const wrappedImagesInPromises = images.map(img=>
-		new Promise((resolve, reject) =>
-			writeOneImageToDisk(img)
-				.then(resolve)
-				.catch(reject))
-	);
-
-	return Promise.all(wrappedImagesInPromises);
-};
-
 export const addImages = (imagesCount, productId) => {
 	if (!imagesCount) {
 		return Promise.reject("No images has been defined");
 	}
 
-	const wrappedImagesInPromises = (new Array(imagesCount)).map(() =>
-		new Promise((resolve, reject) =>
-			addOneImage(productId)
-				.then(resolve)
-				.catch(reject))
-	);
+	const wrappedImagesInPromises = (new Array(imagesCount)).fill(Promise.resolve(addOneImage(productId)));
 
 	return Promise.all(wrappedImagesInPromises)
-		.then(() => productId);
+		.then(imagesIds => imagesIds);
 };
 
--- server/api/utils/utils.js
+++ server/api/utils/utils.js
@@ -3,4 +3,10 @@ export const sendJsonResponse = (res, status, content) => {
 	res.json(content);
 };
 
-export const mapArraysSequentially = (arr1, arr2) => func =>  arr1.map((val, index) => func(arr1[index], arr2[index]));
+export const mapArraysSequentially = (arr1 = [], arr2 = []) => {
+	if (arr1.length > arr2.length) {
+		[arr1, arr2] = [arr2, arr1];
+	}
+
+	return func => arr1.map((val, index) => func(val, arr2[index]));
+};
