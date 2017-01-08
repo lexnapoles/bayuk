309a83c008ab8994d32234aa7b7ce99dc36f4989
Alejandro Napoles <alejandro.napoles@gmail.com>
2017-01-08 20:53:10 +0100

    FormWrapper passes all the properties it doesn't need to the children. FormWrapper submits the form if it doesn't find an error message after validating.

--- test/components/form/formWrapper_test.js
+++ test/components/form/formWrapper_test.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {shallow} from "enzyme";
 import {createDefaultObjectFrom} from "../../../src/utils/utils";
 import {omit} from "lodash/object";
+import {some} from "lodash/collection";
 
 const FormWrapper = WrappedComponent => {
 	const FormContainer = class FormContainer extends Component {
@@ -18,6 +19,7 @@ const FormWrapper = WrappedComponent => {
 			});
 
 			this.handlerWrapper = this.handlerWrapper.bind(this);
+			this.submitForm = this.submitForm.bind(this);
 		}
 
 		defaultHandler(event) {
@@ -43,9 +45,7 @@ const FormWrapper = WrappedComponent => {
 
 		getHandler(handlerName, elementName) {
 			const customHandler = this.props.handlers[handlerName],
-						handler       = customHandler
-							? customHandler
-							: this.defaultHandler;
+						handler       = customHandler	? customHandler	: this.defaultHandler;
 
 			return data => this.handlerWrapper(elementName, handler, data);
 		}
@@ -59,33 +59,94 @@ const FormWrapper = WrappedComponent => {
 			}), {});
 		}
 
-		render() {
-			const {form, errors, handlers} = this.state,
-						props                    = omit(this.props, ["elements", "handlers"]);
+		createErrors(formData, validation, errMsg) {
+			const elements = Object.keys(formData);
 
-			return <WrappedComponent form={form} errors={errors} {...handlers} {...props}/>;
+			const errors = elements.reduce((obj, name) => {
+				const value   = formData[name],
+							isValid = validation[name];
+
+				return {
+					...obj,
+					[name]: isValid(value) ? "" : errMsg[name]
+				};
+			}, {});
+
+			return errors;
+		}
+
+		errorsExist(errors) {
+			return some(errors, error => error.length);
+		}
+
+		validate(form) {
+			const {validation, errorMessages} = this.props,
+						errors                      = this.createErrors(form, validation, errorMessages);
+
+			if (this.errorsExist(errors)) {
+				this.setState({errors});
+
+				return false;
+			}
+
+			return true;
+		}
+
+		submitForm(event) {
+			event.preventDefault();
+
+			const {form} = this.state;
+
+			if (!this.validate(form)) {
+				return;
+			}
+
+			this.props.onSubmit(form);
+		}
+
+		render() {
+			const ownProps                 = ["elements", "onSubmit", "errorMessages", "validation", "handlers"],
+						props                    = omit(this.props, ownProps),
+						{form, errors, handlers} = this.state;
+
+			return <WrappedComponent form={form} errors={errors} onSubmit={this.submitForm}{...handlers} {...props} />;
 		}
 	};
 
 	FormContainer.propTypes = {
-		elements: React.PropTypes.array.isRequired,
-		handlers: React.PropTypes.object
+		elements:      React.PropTypes.array.isRequired,
+		onSubmit: 		 React.PropTypes.func.isRequired,
+		errorMessages: React.PropTypes.object.isRequired,
+		validation:    React.PropTypes.object.isRequired,
+		handlers:      React.PropTypes.object
 	};
 
 	FormContainer.defaultProps = {
-		handlers: {}
+		handlers:      {}
 	};
 
 	return FormContainer;
 };
 
+const getEvent = value => ({
+	target: {
+		value
+	}
+});
+
 const getFormComponent = (children = React.Component) => FormWrapper(children);
 
 const renderForm = (props, children) => {
-	const Form = getFormComponent(children);
+	const Form          = getFormComponent(children),
+				validation    = {},
+				errorMessages = {},
+				onSubmit      = () => void 0;
 
-	return shallow(<Form {...props}/>);
+	const formProps = {validation, errorMessages, onSubmit, ...props};
+
+	return shallow(<Form {...formProps}/>);
 };
+
 
 describe("<FormWrapper/>", function () {
 	it("creates the form state using the elements array", function () {
@@ -121,31 +182,25 @@ describe("<FormWrapper/>", function () {
 		assert.equal(defaultHandler(event), value);
 	});
 
-	it("assigns, by default, the default behaviour to the handlers in the state", function () {
+	it("assigns, by default, a default behaviour to the handlers in the state", function () {
 		const wrapper        = renderForm({elements: ["name"]}),
 					defaultHandler = wrapper.state().handlers["onNameChange"],
 					value          = "George";
 
-		const event = {
-			target: {
-				value
-			}
-		};
+		Reflect.apply(defaultHandler, wrapper, [getEvent(value)]);
 
-		Reflect.apply(defaultHandler, wrapper, [event]);
-
-		assert.equal(wrapper.state().form.name, value);
+		assert.equal(wrapper.state("form").name, value);
 	});
 
 	it("adds a custom handler instead of the default if it finds one", function () {
 		const customMessage = " a custom handler",
 					handlers      = {onEmailChange: () => customMessage},
 					wrapper       = renderForm({elements: ["email"], handlers}),
-					onEmailChange = wrapper.state().handlers["onEmailChange"];
+					onEmailChange = wrapper.state("handlers")["onEmailChange"];
 
 		Reflect.apply(onEmailChange, wrapper, []);
 
-		assert.equal(wrapper.state().form.email, customMessage);
+		assert.equal(wrapper.state("form").email, customMessage);
 	});
 
 	it("passes the form state to the children", function () {
@@ -157,7 +212,7 @@ describe("<FormWrapper/>", function () {
 
 	it("passes the errors state to the children", function () {
 		const Children = () => <div></div>,
-					wrapper  = renderForm({elements: ["name"]}, Children);
+					wrapper  = renderForm({elements: ["name", "email"]}, Children);
 
 		assert.deepEqual(wrapper.find("Children").prop("errors"), {name: "", email: ""})
 	});
@@ -167,6 +222,45 @@ describe("<FormWrapper/>", function () {
 					wrapper  = renderForm({elements: ["name"]}, Children);
 
 		assert.isFunction(wrapper.find("Children").prop("onNameChange"));
+	});
+
+	it("passes all the properties it doesn't need to children", function () {
+		const Children = () => <div></div>,
+					wrapper  = renderForm({elements: ["name"], unnecessaryProp: "unnecessary"}, Children);
+
+		assert.isDefined(wrapper.find("Children").prop("unnecessaryProp"));
+	});
+
+	it("doesn't pass own properties to children", function () {
+		const Children = () => <div></div>,
+					wrapper  = renderForm({elements: ["name"], validation: {prop: "unnecessary"}}, Children);
+
+		assert.isUndefined(wrapper.find("Children").prop("validation"));
+	});
+
+	it("creates the errors validating the form data with a validation and errorMessages object", function () {
+		const createErrors  = getFormComponent().prototype.createErrors,
+					formData      = {name: "Steven"},
+					validation    = {name: value => value === "George"},
+					errorMessages = {name: "Invalid name"};
+
+		const errors = createErrors(formData, validation, errorMessages);
+
+	assert.equal(errors.name, errorMessages.name);
+	});
+
+	it("submits the form if it doesn't find an error message after validating", function () {
+		const onSubmit      = sinon.spy(),
+					validation    = {name: value => value === "George"},
+					errorMessages = {name: "Invalid name"},
+					wrapper       = renderForm({elements: ["name"], validation, errorMessages, onSubmit}),
+					onNameChange  = wrapper.state("handlers")["onNameChange"];
+
+		Reflect.apply(onNameChange, wrapper, [getEvent("George")]);
+
+		wrapper.instance().submitForm(new Event("submit"));
+
+		assert.isTrue(onSubmit.called);
 	});
 });
 
