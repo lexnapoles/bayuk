90e7295d067a2dcd06c5a8acbc71b3cc6174c969
Alejandro Napoles <alejandro.napoles@gmail.com>
2017-05-11 14:20:23 +0200

    Moved productNotFoundError middlware logic to verifyProduct middleware

--- server/middlewares/verifyProduct.js
+++ server/middlewares/verifyProduct.js
@@ -2,13 +2,8 @@ import {has} from "lodash/object";
 import {getProductById} from "../api/services/products";
 import {sendJsonResponse} from "../../utils/utils";
 import {userDoesNotExist} from "../errors/api/userErrors";
+import {productDoesNotExist} from "../errors/api/productErrors";
 import {tokenDoesNotMatch} from "../errors/api/authorizationErrors";
-
-const ProductNotFoundError = () => ({
-	name:    "ProductNotFoundError",
-	message: "Product not found"
-});
-
 
 export default (req, res, next) => {
 	if (!has(req, "user")) {
@@ -16,15 +11,15 @@ export default (req, res, next) => {
 		return;
 	}
 
-
 	if (!has(req, "params") || !has(req.params, "productId")) {
-		return next(ProductNotFoundError());
+		sendJsonResponse(res, 404, [productDoesNotExist()]);
+		return;
 	}
 
 	const tokenUserId = req.user.id,
 				productId   = req.params.productId;
 
 	getProductById(productId)
-		.then(({owner}) => owner !== tokenUserId ? 	sendJsonResponse(res, 403, [tokenDoesNotMatch()]) : next())
-		.catch(() => next(ProductNotFoundError()));
+		.then(({owner}) => owner !== tokenUserId ? sendJsonResponse(res, 403, [tokenDoesNotMatch()]) : next())
+		.catch(() => sendJsonResponse(res, 404, [productDoesNotExist()]));
 };
