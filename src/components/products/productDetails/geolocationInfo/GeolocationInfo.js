bfcd7e39c77d3c3e13480a245a8ab4bec6931874
Alejandro Napoles <alejandro@alejandronapoles.com>
2017-07-06 20:36:02 +0200

    Turned GeolocationInfo component  into a stateless function.

--- src/components/products/productDetails/geolocationInfo/GeolocationInfo.js
+++ src/components/products/productDetails/geolocationInfo/GeolocationInfo.js
@@ -1,25 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {container, map} from "./geolocationInfo.css";
 
-class GeolocationInfo extends Component {
-	render() {
-		const {className, style, latitude, longitude} = this.props;
-
-		const mapUrl = `https://maps.googleapis.com/maps/api/staticmap
+const GeolocationInfo = ({className, style, latitude, longitude}) => {
+	const mapUrl = `https://maps.googleapis.com/maps/api/staticmap
 		?center="${latitude.toFixed(4)},${longitude.toFixed(4)}"
 		&zoom=14
 		&size=500x200
 		&maptype=roadmap
 		&key=AIzaSyCyX0Ciq1pkgNMAYA6GSDn2x0luKtACTqI`;
 
-		return (
-			<div className={`${container} ${className}`} style={style}>
-				<img src={mapUrl} className={map}/>
-			</div>
-		)
-	}
-}
+	return (
+		<div className={`${container} ${className}`} style={style}>
+			<img src={mapUrl} className={map}/>
+		</div>
+	)
+};
 
 GeolocationInfo.propTypes = {
 	latitude:  PropTypes.number.isRequired,
